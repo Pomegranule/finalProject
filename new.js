1bf11369a74c8c27044f6aaf6d0b892b7d3b3e6a
Pomegranule <elleg1@gmx.com>
2021-05-20 15:06:12 +0100

    Welp. Looks like you messed it up.
    It was a mess from the start. You COULD go back to the last commit, but only God can save you now, lmao.

--- new.js
+++ new.js
@@ -2,35 +2,75 @@
 
 
 
-let taskName = document.getElementById("nameOfTask");
+// let taskName = document.getElementById("nameOfTask");
 
-taskName.addEventListener("input", function () {
-  if (taskName.value.length > 20) {
-    taskName.setCustomValidity("Cannot be more than 20 characters!");
-  } else {
-    taskName.setCustomValidity("");
+// taskName.addEventListener("input", function () {
+//   if (taskName.value.length > 20) {
+//     taskName.setCustomValidity("Cannot be more than 20 characters!");
+//   } else {
+//     taskName.setCustomValidity("");
+//   }
+// });
+
+// let taskDesc = document.getElementById("taskDescription");
+
+// taskDesc.addEventListener("input", function () {
+//   if ((taskDesc.value.length > 60) || (taskDesc.value.length < 0)) {
+//     taskDesc.setCustomValidity("Cannot be more than 60 characters!");
+//   } else {
+//     taskDesc.setCustomValidity("");
+//   }
+// });
+
+// let taskName = document.getElementById("nameOfTask");
+
+// taskName.addEventListener("input", function () {
+//   if (taskName.value.length > 20) {
+//     taskName.setCustomValidity("Cannot be more than 20 characters!");
+//   } else {
+//     taskName.setCustomValidity("");
+//   }
+// });
+
+// function newTask(nameOfTask, taskDescription, assignTask, dueDate, status) {
+//   let newObject = {
+    
+//   }
+// }
+
+class TaskManagement {
+  constructor(){
+    this.tasks = {}
+    this.id = 0;
+    
   }
-});
-
-let taskDesc = document.getElementById("taskDescription");
-
-taskDesc.addEventListener("input", function () {
-  if (taskDesc.value.length > 60) && (taskDesc.value.length < 0)) {
-    taskDesc.setCustomValidity("Cannot be more than 60 characters!");
-  } else {
-    taskDesc.setCustomValidity("");
+  
+  createTaskObject (nameOfTask, taskDescription, assignTask, dueDate, status) {
+    let newTask = new Task(nameOfTask, taskDescription, assignTask, dueDate, status);
+    this.tasks[this.id] = newTask;
+    this.id++;
   }
-});
 
-let taskName = document.getElementById("nameOfTask");
-
-taskName.addEventListener("input", function () {
-  if (taskName.value.length > 20) {
-    taskName.setCustomValidity("Cannot be more than 20 characters!");
-  } else {
-    taskName.setCustomValidity("");
+  saveTaskChanges () {
+    localStorage.setItem("tasks", JSON.stringify);
   }
-});
+}
+  // removeTask () {
+
+  // }
+
+
+class Task {
+  constructor(nameOfTask, newId, taskDescription, assignTask, dueDate, status) {
+    this.taskName = nameOfTask + newId;
+    this.taskDesc = taskDescription;
+    this.assignedTo = assignTask;
+    this.date = dueDate;
+    this.statusUpdate = status;
+  }
+}
+
+
 
 // class Task {
 //     constructor(id, name, description, assigned, due, status){
